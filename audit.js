2c83a9903cc012beb2e0569cc72f96d35d766cdb
Thomas Brace <thomas.brace@komododigital.co.uk>
2017-09-04 17:02:58 +0100

    Fixed Following Bugs: Getting Href from page, Validating a page with a 404 error
    Addded the following:
    Parallelization with lighthouse reports so overall they are performed quicker

--- audit.js
+++ audit.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const flags = {};
 
 async function getPagesInitial(url,dir){
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
+	var browser = await puppeteer.launch();
+	var page = await browser.newPage();
 	page.setViewport({width: 1920, height: 1080});
 
 	if (!fs.existsSync(dir)){
@@ -17,18 +17,29 @@ async function getPagesInitial(url,dir){
     	fs.mkdirSync(dir+'/markupValidator');
 	}
 
-	if (url.slice(-1)==="/")
-	{
-		url = url.slice(0,-1);
-	}
 	await page.goto(url);
 	var pageList =[];
 	pageList = await getPages("/",pageList,url,"/",page);
 	browser.close();
-	createLighthouseReports(pageList,dir,url).catch(console.error.bind(console));
-	screenshots(pageList,dir,url).catch(console.error.bind(console));
+	return pageList;
 }
 
+async function getHref(j,browserPage)
+{
+	var tempHref = await browserPage.evaluate((j) => {
+		var x = document.querySelectorAll('a')[j];
+		if (x != undefined)
+		{
+			if (x != undefined)
+			{
+				return x.href;
+			}
+			else {return 'undefined';}
+		}
+		else {return 'undefined';}
+	},j);
+	return tempHref;
+}
 async function getPages(page,pageList,addr,pageBefore,browserPage)
 {
 	// if (page.indexOf("insights") !== -1)
@@ -44,21 +55,36 @@ async function getPages(page,pageList,addr,pageBefore,browserPage)
 		// {
 		// 	await browserPage.waitFor(1000);
 		// }
-		console.log('start');
-		console.log(j);
-		var tempHref = await browserPage.evaluate((j) => {
-			x = document.querySelectorAll('a')[j];
-			if (x != null)
-			{
-				return x.href;
+		try {
+			var tempHref = await getHref(j,browserPage);
+		}
+		catch(e)
+		{
+			console.log("Catch1");
+			try {
+				var tempHref = await getHref(j,browserPage);
 			}
-			else {return 'undefined';}
-		},j);
-		// var tempAnchors = await browserPage.$$('a');
-		// var tempHref = await (tempAnchors[j]).attribute('href');
-		console.log('finish');
+			catch(e)
+			{
+				console.log("Catch2");
+				try {
+					var tempHref = await getHref(j,browserPage);
+				}
+				catch(e)
+				{
+					console.log("Catch3");
+					try {
+						var tempHref = await getHref(j,browserPage);
+					}
+					catch(e)
+					{
+						throw "Get HREF of element failed";
+					}
+				}
+			}
+		}
 		//check if the link is in the same domain as the initial address
-		tempHref = tempHref.replace("#","");
+		tempHref = tempHref.split("#")[0];
 		var newPage = tempHref.replace(addr,"");
 		if ((tempHref!==newPage) && (pageList.indexOf(newPage) === -1))
 		{
@@ -69,7 +95,6 @@ async function getPages(page,pageList,addr,pageBefore,browserPage)
 			pageList = await getPages(newPage,pageList,addr,page,browserPage);
 		}
 	}
-	//console.log(page);
 	await browserPage.goto(addr+pageBefore);
 	return pageList;
 }
@@ -82,68 +107,98 @@ function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
   });
 }
 
-async function screenshots(listOfPages,dir,url)
+async function parallelScreenshots(listOfPages,dir,url,j,parallelNum)
 {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.setViewport({width: 1920, height: 1080});
-	for (var i=0;i<listOfPages.length;i++)
+	var browser = await puppeteer.launch();
+	var page = await browser.newPage();
+	var pagesLength = listOfPages.length;
+	for (var i=0;i<Math.ceil(pagesLength/parallelNum);i++)
 	{
-		if (listOfPages[i].slice(-4) !== '.pdf')
+		var arrayNum = (i*parallelNum)+j;
+		console.log(arrayNum);
+		if (arrayNum < pagesLength)
 		{
-			pageName = listOfPages[i].replace(/\//g,'_');
-			pageName = pageName.replace(/#/g,'');
-			//console.log(pageName);
-			await page.goto(url+listOfPages[i]);
-			await page.screenshot({path: dir+'/images/'+pageName+'.png', fullPage: true});
+			if (listOfPages[arrayNum].slice(-4) !== '.pdf')
+			{
+				await page.setViewport({width: 1920, height: 1080});
+				pageName = listOfPages[arrayNum].replace(/\//g,'_');
+				pageName = pageName.replace(/#/g,'');
+				await page.goto(url+listOfPages[arrayNum]);
+				await page.screenshot({path: dir+'/images/'+pageName+'.png', fullPage: true});
 
-			await page.goto('https://validator.w3.org/');
-			await page.focus('#uri');
-			await page.type(url+listOfPages[i]);
-			await page.click('#validate-by-uri .submit');
-			await page.waitForSelector('.details');
-			await page.screenshot({path: dir+'/markupValidator/'+pageName+'.png', fullPage: true});
-		}
-	}
-	await page.setViewport({width: 320, height: 568, isMobile: true});
-	for (var i=0;i<listOfPages.length;i++)
-	{
-		if (listOfPages[i].slice(-4) !== '.pdf')
-		{
-			pageName = listOfPages[i].replace(/\//g,'_');
-			pageName = pageName.replace(/#/g,'');
-			//console.log(pageName);
-			await page.goto(url+listOfPages[i]);
-			await page.screenshot({path: dir+'/mobileImages/'+pageName+'.png', fullPage: true});
+				await page.goto('https://validator.w3.org/');
+				await page.focus('#uri');
+				await page.type(url+listOfPages[arrayNum]);
+				await page.click('#validate-by-uri .submit');
+				await page.waitFor(3000);
+				await page.screenshot({path: dir+'/markupValidator/'+pageName+'.png', fullPage: true});
+
+				await page.setViewport({width: 320, height: 568, isMobile: true});
+				await page.goto(url+listOfPages[arrayNum]);
+				await page.screenshot({path: dir+'/mobileImages/'+pageName+'.png', fullPage: true});
+			}
 		}
 	}
 	browser.close();
 }
 
-async function createLighthouseReports(allPages,dir,url)
+function createScreenshots(listOfPages,dir,url)
 {
-	for (var i=0;i<allPages.length;i++)
+	parallelNum = 8;
+	for (var j=0;j<parallelNum;j++)
 	{
-		await launchChromeAndRunLighthouse(url+allPages[i], flags).then(results => {
-			var pageName = allPages[i].replace(/\//g,'_');		
-			pageName = pageName.replace(/#/g,'');
-			fs.writeFile(dir+'/lighthouse/'+pageName+'_report.JSON', JSON.stringify(results), (err) => {
-			  	if (err) throw err;
-			  	//console.log('The file has been saved!');
-			});
-		});
+		parallelScreenshots(listOfPages,dir,url,j,parallelNum).catch(console.error.bind(console));
 	}
 }
 
-if((process.argv.indexOf("--url") != -1) && (process.argv.indexOf("--dir") != -1)){ //does our flag exist?
-    var url = process.argv[process.argv.indexOf("--url") + 1]; //grab the next item
-    var dir = process.argv[process.argv.indexOf("--dir") + 1]; //grab the next item
-    getPagesInitial(url,dir).catch(console.error.bind(console))
-}
-else
+async function parallelLighthouseReports(allPages,dir,url,j,parallelNum)
 {
-	console.log("		**************************************************");
-	console.log("		URL needs to be passed using the following syntax");
-	console.log("		node audit.js --url <URL> --dir <Directory Name>");
-	console.log("		**************************************************");
+	var pagesLength = allPages.length;
+	for (var i=0;i<Math.ceil(pagesLength/parallelNum);i++)
+	{
+		var arrayNum = (i*parallelNum)+j;
+		if (arrayNum < pagesLength)
+		{
+			await launchChromeAndRunLighthouse(url+allPages[arrayNum], flags).then(results => {
+				var pageName = allPages[arrayNum].replace(/\//g,'_');	
+				pageName = pageName.replace(/#/g,'');
+				fs.writeFile(dir+'/lighthouse/'+pageName+'_report.JSON', JSON.stringify(results), (err) => {
+				  	if (err) throw err;
+				  	//console.log('The file has been saved!');
+				});
+			});
+		}
+	}
 }
+function createLighthouseReports(allPages,dir,url)
+{
+	parallelNum = 4;
+	for (var j=0;j<parallelNum;j++)
+	{
+		parallelLighthouseReports(allPages,dir,url,j,parallelNum).catch(console.error.bind(console));
+	}
+}
+
+async function audit()
+{
+	if((process.argv.indexOf("--url") != -1) && (process.argv.indexOf("--dir") != -1)){ //does our flag exist?
+	    var url = process.argv[process.argv.indexOf("--url") + 1]; //grab the next item
+	    if (url.slice(-1)==="/")
+		{
+			url = url.slice(0,-1);
+		}
+	    var dir = process.argv[process.argv.indexOf("--dir") + 1]; //grab the next item
+	    var pageList = await getPagesInitial(url,dir).catch(console.error.bind(console));
+	    console.log(pageList);
+	    createLighthouseReports(pageList,dir,url);
+		parallelScreenshots(pageList,dir,url,0,1).catch(console.error.bind(console));
+	}
+	else
+	{
+		console.log("**************************************************");
+		console.log("URL needs to be passed using the following syntax");
+		console.log("node audit.js --url <URL> --dir <Directory Name>");
+		console.log("**************************************************");
+	}
+}
+audit().catch(console.error.bind(console));
