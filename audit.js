e8a73a780e18806bb72d77168b2aab87b81dcf99
Thomas Brace <thomas.brace@komododigital.co.uk>
2017-09-05 16:48:25 +0100

    Cleanup Audit.js

--- audit.js
+++ audit.js
@@ -44,48 +44,48 @@ async function bfsGetPages(firstPage,url)
 	return pageList;
 }
 
-async function parallelScreenshots(listOfPages,dir,url,j,parallelNum)
+async function takeScreenshot(page,listOfPages,i,pageName,url,dir)
+{
+	await page.setViewport({width: 1920, height: 1080});
+	await page.goto(url+listOfPages[i]);
+	await page.screenshot({path: dir+'/images/'+pageName+'.png', fullPage: true});
+}
+
+async function takeMarkupScreenshot(page,listOfPages,i,pageName,url,dir)
+{
+	await page.setViewport({width: 1920, height: 1080});
+	await page.goto('https://validator.w3.org/');
+	await page.focus('#uri');
+	await page.type(url+listOfPages[i]);
+	await page.click('#validate-by-uri .submit');
+	await page.waitFor(3000);
+	await page.screenshot({path: dir+'/markupValidator/'+pageName+'.png', fullPage: true});
+}
+
+async function takeMobileScreenshot(page,listOfPages,i,pageName,url,dir)
+{
+	await page.setViewport({width: 320, height: 568, isMobile: true});
+	await page.goto(url+listOfPages[i]);
+	await page.screenshot({path: dir+'/mobileImages/'+pageName+'.png', fullPage: true});
+}
+
+async function createScreenshots(listOfPages,dir,url)
 {
 	var browser = await puppeteer.launch();
 	var page = await browser.newPage();
 	var pagesLength = listOfPages.length;
-	for (var i=0;i<Math.ceil(pagesLength/parallelNum);i++)
+	for (var i=0;i<pagesLength;i++)
 	{
-		var arrayNum = (i*parallelNum)+j;
-		console.log(arrayNum);
-		if (arrayNum < pagesLength)
+		if (listOfPages[i].slice(-4) !== '.pdf')
 		{
-			if (listOfPages[arrayNum].slice(-4) !== '.pdf')
-			{
-				await page.setViewport({width: 1920, height: 1080});
-				var pageName = listOfPages[arrayNum].replace(/\//g,'_');
-				pageName = pageName.replace(/#/g,'');
-				await page.goto(url+listOfPages[arrayNum]);
-				await page.screenshot({path: dir+'/images/'+pageName+'.png', fullPage: true});
-
-				await page.goto('https://validator.w3.org/');
-				await page.focus('#uri');
-				await page.type(url+listOfPages[arrayNum]);
-				await page.click('#validate-by-uri .submit');
-				await page.waitFor(3000);
-				await page.screenshot({path: dir+'/markupValidator/'+pageName+'.png', fullPage: true});
-
-				await page.setViewport({width: 320, height: 568, isMobile: true});
-				await page.goto(url+listOfPages[arrayNum]);
-				await page.screenshot({path: dir+'/mobileImages/'+pageName+'.png', fullPage: true});
-			}
+			var pageName = listOfPages[i].replace(/\//g,'_');
+			pageName = pageName.replace(/#/g,'');
+			await takeScreenshot(page,listOfPages,i,pageName,url,dir);
+			await takeMarkupScreenshot(page,listOfPages,i,pageName,url,dir);
+			await takeMobileScreenshot(page,listOfPages,i,pageName,url,dir);
 		}
 	}
 	browser.close();
-}
-
-function createScreenshots(listOfPages,dir,url)
-{
-	var parallelNum = 8;
-	for (var j=0;j<parallelNum;j++)
-	{
-		parallelScreenshots(listOfPages,dir,url,j,parallelNum).catch(console.error.bind(console));
-	}
 }
 
 function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
@@ -145,7 +145,7 @@ async function audit()
 		}
 	    var pageList = await bfsGetPages("/",url).catch(console.error.bind(console));
 		createLighthouseReports(pageList,dir,url);
-		parallelScreenshots(pageList,dir,url,0,1).catch(console.error.bind(console));
+		createScreenshots(pageList,dir,url).catch(console.error.bind(console));
 	}
 	else
 	{
